28d7b679d4e4ba45451936bfdfc8bdd40ea2f4f6
huyt <huyt@huyUbuntu.myguest.virtualbox.org>
2024-11-12 21:27:51 -0800

    Assignment 4 part 1-final commit

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,7 +14,28 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+    struct thread_data* param = (struct thread_data *) thread_param;
+	int rc;
+	
+	usleep(param->wait_to_obtain_ms * 1000);
+	rc = pthread_mutex_lock(param->mutex);
+	if (rc !=0){
+		printf("pthread_mutex_lock failed with %d\n", rc);
+		param->thread_complete_success = false;
+		return thread_param;
+	}
+
+	usleep(param->wait_to_release_ms * 1000);
+	
+	rc = pthread_mutex_unlock(param->mutex);
+	if (rc !=0){
+		printf("pthread_mutex_unlock failed with %d\n", rc);
+		param->thread_complete_success = false;
+		return thread_param;
+	}
+
+	param->thread_complete_success = true;
+	return thread_param;
 }
 
 
@@ -28,6 +49,28 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+	struct thread_data *thread_param = (struct thread_data*)malloc(sizeof(struct thread_data));
+	thread_param->mutex = mutex;
+	thread_param->wait_to_obtain_ms = wait_to_obtain_ms;
+	thread_param->wait_to_release_ms = wait_to_release_ms;
+	
+	int rc;
+/*	rc = pthread_mutex_init(thread_param->mutex, NULL);
+	
+	if (rc != 0){
+		printf("pthread_mutex_init failed with %d\n", rc);
+		return false;
+	}*/
+
+	rc = pthread_create(thread,
+				  	   NULL,
+				   	   threadfunc,
+				   	   (void*)thread_param );
+	if (rc != 0){
+		printf("pthread_create failed wtih %d\n", rc);
+		return false;
+	}
+
+	return true; 
 }
 
